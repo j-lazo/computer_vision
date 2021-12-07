e2203d22ed725460b09c941c6620a62030d1b92e
j-lazo <j.org.ls@ciencias.unam.mx>
2021-12-07 10:15:39 +0100

    analyze data

---
+++ scripts/analyze_data.py
@@ -0,0 +1,26 @@
+import os
+from absl import app, flags, logging
+from absl.flags import FLAGS
+from general_functions import data_management as dam
+from general_functions import data_management as daa
+from matplotlib import pyplot as plt
+import cv2
+import classification as imgclass
+from segmentation import call_models as imseg
+
+
+flags.DEFINE_string('path_dir', '', 'path to directory')
+
+
+def run_experiment(_argv):
+
+    if FLAGS.path_dir:
+        dam.generate_training_and_validation_sets(FLAGS.path_dir)
+
+
+
+if __name__ == '__main__':
+    try:
+        app.run(run_experiment)
+    except SystemExit:
+        pass
