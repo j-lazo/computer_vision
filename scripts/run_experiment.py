ea25d32ddd6281cbfad6303463c64f537ef62e04
j-lazo <j.org.ls@ciencias.unam.mx>
2022-01-30 16:51:32 +0100

    run experiment Flagas added

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -5,27 +5,28 @@ from general_functions import data_management as dam
 from matplotlib import pyplot as plt
 import cv2
 from classification import call_models as img_class
-flags.DEFINE_string('data_dir', '', 'path to directory')
+
+DATASETS = ['tissue_classification']
 
 
 def run_experiment(_argv):
+    #base_dir = os.path.normpath(os.getcwd())
+    #path_list = base_dir.split(os.sep)
+    # data_dir = os.path.join(*path_list[:-1], 'datasets/tissue_classification/')
 
     dataset = 'tissue_classification'
-    name_model = 'fc_3layers'
-    backbone_model = 'resnet101'
-    mode = 'train_backbone'
-    batch_zie = 8
-    learning_rate = 0.001
-    epochs = 7
-    base_dir = os.path.normpath(os.getcwd())
-    path_list = base_dir.split(os.sep)
     data_dir = ''.join([os.getcwd(), '/datasets/', dataset, '/'])
-    #data_dir = os.path.join(*path_list[:-1], 'datasets/tissue_classification/')
     test_data = data_dir + 'test/'
     results_dir = data_dir + 'results/'
-    analyze_data = True
-    trainable_layers = - 7
-    fine_tune_epochs = 3
+
+    name_model = FLAGS.name_model
+    mode = FLAGS.mode
+    backbone_model = FLAGS.backbone
+    batch_zie = FLAGS.batch_size
+    epochs = FLAGS.epochs
+    trainable_layers = FLAGS.trainable_layers
+    fine_tune_epochs = FLAGS.fine_tune_epochs
+    analyze_data = FLAGS.analyze_data
 
     if mode == 'train_backbone':
         learning_rate = [1e-5, 0.001]
@@ -42,6 +43,26 @@ def run_experiment(_argv):
 
 
 if __name__ == '__main__':
+
+    flags.DEFINE_string('name_model', 'fc_3layers', 'name of the model')
+    flags.DEFINE_string('mode', 'train_backbone', 'train, predict, train_backbone')
+    flags.DEFINE_string('backbone', 'resnet50', 'backbone network')
+    flags.DEFINE_integer('batch_size', 8, 'batch size')
+    flags.DEFINE_float('learning_rate', 0.001, 'learning rate')
+    flags.DEFINE_integer('epochs', 7, 'number of epochs')
+    flags.DEFINE_integer('trainable_layers', -7, 'Trainable layers in case backbone is trained')
+    flags.DEFINE_bool('analyze_data', True, 'select if analyze data or not')
+    flags.DEFINE_integer('fine_tune_epochs', 3, 'epochs to fine tune the model')
+
+    flags.DEFINE_string('dataset_dir', os.getcwd() + 'data/', 'path to dataset')
+    flags.DEFINE_string('val_dataset', '', 'path to validation dataset')
+    flags.DEFINE_string('test_dataset', '', 'path to test dataset')
+    flags.DEFINE_string('results_dir', os.getcwd() + 'results/', 'path to dataset')
+    flags.DEFINE_string('weights', '', 'path to weights file')
+    flags.DEFINE_string('directory_model', '', 'indicate the path to the directory')
+    flags.DEFINE_float('validation_split', 0.2, 'iif not validation dir but needed')
+    flags.DEFINE_string('file_to_predic', '', 'Directory or file where to perform predictions if predict mode selected')
+
     try:
         app.run(run_experiment)
     except SystemExit:
