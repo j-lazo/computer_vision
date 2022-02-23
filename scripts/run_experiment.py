5a2bea45f3678e5e6002ca7bc5add73b9fc33c88
j-lazo <j.org.ls@ciencias.unam.mx>
2022-02-23 10:55:26 +0100

    added in DATASETS bladder_dataset_v2_augmented

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -9,7 +9,8 @@ from classification import call_models as img_class
 from segmentation import call_models as segm
 from classification import grad_cam as gc
 
-DATASETS = ['tissue_classification', 'kvasir_image_classification', 'bladder_tissue_classification', 'bladder_tissue_classification_v2']
+DATASETS = ['tissue_classification', 'kvasir_image_classification', 'bladder_tissue_classification',
+            'bladder_tissue_classification_v2', 'bladder_tissue_classification_v2_augmented']
 
 flags.DEFINE_string('experiment_type', '', 'experiment type')
 flags.DEFINE_string('name_model', 'fc_3layers', 'name of the model')
