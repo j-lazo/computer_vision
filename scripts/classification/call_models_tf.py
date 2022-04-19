ea3bc8eb756a6512f2941854fee29c01ed68f590
j-lazo <j.org.ls@ciencias.unam.mx>
2022-04-19 11:45:12 +0200

    double the number of neurons in the last 3 fc layers of the combined model

--- scripts/classification/call_models_tf.py
+++ scripts/classification/call_models_tf.py
@@ -327,6 +327,93 @@ def load_pretrained_backbones(name_model, weights='imagenet', include_top=False,
     return new_base_model
 
 
+def multi_output_model(backbones=['resnet101', 'resnet101', 'resnet101'], after_concat='globalpooling',
+                dropout=False):
+
+    input_sizes_models = {'vgg16': (224, 224), 'vgg19': (224, 224), 'inception_v3': (299, 299),
+                          'resnet50': (224, 224), 'resnet101': (224, 244), 'mobilenet': (224, 224),
+                          'densenet121': (224, 224), 'xception': (299, 299)}
+
+    num_backbones = len(backbones)
+    input_model = Input((3, 256, 256, 3))
+    x1, x2, x3 = tf.split(input_model, 3, axis=1)
+    input_backbone_1 = tf.squeeze(x1, axis=1)
+    input_backbone_2 = tf.squeeze(x2, axis=1)
+    input_backbone_3 = tf.squeeze(x3, axis=1)
+
+    b1 = tf.image.resize(input_backbone_1, input_sizes_models[backbones[0]], method='bilinear')
+    if backbones[0] == 'resnet101':
+        b1 = tf.keras.applications.resnet.preprocess_input(b1)
+    elif backbones[0] == 'resnet50':
+        b1 = tf.keras.applications.resnet50.preprocess_input(b1)
+    elif backbones[0] == 'densenet121':
+        b1 = tf.keras.applications.densenet.preprocess_input(b1)
+    elif backbones[0] == 'vgg19':
+        b1 = tf.keras.applications.vgg19.preprocess_input(b1)
+    elif backbones[0] == 'inception_v3':
+        b1 = tf.keras.applications.inception_v3.preprocess_input(b1)
+
+    backbone_model_1 = load_pretrained_backbones(backbones[0])
+    backbone_model_1._name = 'backbone_1'
+    for layer in backbone_model_1.layers:
+        layer.trainable = False
+    b1 = backbone_model_1(b1)
+
+    b2 = tf.image.resize(input_backbone_2, input_sizes_models[backbones[1]], method='bilinear')
+    if backbones[1] == 'resnet101':
+        b2 = tf.keras.applications.resnet.preprocess_input(b2)
+    elif backbones[1] == 'resnet50':
+        b2 = tf.keras.applications.resnet50.preprocess_input(b2)
+    elif backbones[1] == 'densenet121':
+        b2 = tf.keras.applications.densenet.preprocess_input(b2)
+    elif backbones[1] == 'vgg19':
+        b2 = tf.keras.applications.vgg19.preprocess_input(b2)
+    elif backbones[1] == 'inception_v3':
+        b2 = tf.keras.applications.inception_v3.preprocess_input(b2)
+    backbone_model_2 = load_pretrained_backbones(backbones[1])
+    backbone_model_2._name = 'backbone_2'
+    for layer in backbone_model_2.layers:
+        layer.trainable = False
+    b2 = backbone_model_2(b2)
+    if num_backbones == 3:
+        b3 = tf.image.resize(input_backbone_3, input_sizes_models[backbones[2]], method='bilinear')
+        if backbones[2] == 'resnet101':
+            b3 = tf.keras.applications.resnet.preprocess_input(b3)
+        elif backbones[2] == 'resnet50':
+            b3 = tf.keras.applications.resnet50.preprocess_input(b3)
+        elif backbones[2] == 'densenet121':
+            b3 = tf.keras.applications.densenet.preprocess_input(b3)
+        elif backbones[2] == 'vgg19':
+            b3 = tf.keras.applications.vgg19.preprocess_input(b3)
+        elif backbones[2] == 'inception_v3':
+            b3 = tf.keras.applications.inception_v3.preprocess_input(b3)
+        backbone_model_3 = load_pretrained_backbones(backbones[2])
+        backbone_model_3._name = 'backbone_3'
+        for layer in backbone_model_3.layers:
+            layer.trainable = False
+        b3 = backbone_model_3(b3)
+        x = Concatenate()([b1, b2, b3])
+
+    else:
+        x = Concatenate()([b1, b2])
+    if after_concat == 'globalpooling':
+        x = GlobalAveragePooling2D()(x)
+    else:
+        x = Flatten()(x)
+    x = Dense(1024, activation='relu')(x)
+    if dropout:
+        x = Dropout(0.5)(x)
+    x = Dense(2048, activation='relu')(x)
+    if dropout:
+        x = Dropout(0.5)(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Flatten()(x)
+    output_layer = Dense(5, activation='softmax')(x)
+
+    return Model(inputs=input_model, outputs=output_layer, name='multi_input_classification')
+
+
+
 def build_model(backbones=['resnet101', 'resnet101', 'resnet101'], after_concat='globalpooling',
                 dropout=False):
 
@@ -400,13 +487,13 @@ def build_model(backbones=['resnet101', 'resnet101', 'resnet101'], after_concat=
         x = GlobalAveragePooling2D()(x)
     else:
         x = Flatten()(x)
-    x = Dense(512, activation='relu')(x)
-    if dropout:
-        x = Dropout(0.5)(x)
     x = Dense(1024, activation='relu')(x)
     if dropout:
         x = Dropout(0.5)(x)
-    x = Dense(1024, activation='relu')(x)
+    x = Dense(2048, activation='relu')(x)
+    if dropout:
+        x = Dropout(0.5)(x)
+    x = Dense(2048, activation='relu')(x)
     x = Flatten()(x)
     output_layer = Dense(5, activation='softmax')(x)
 
