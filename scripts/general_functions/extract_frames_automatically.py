446aec9275587a9e1176cd00a72bdb6a02db9d7c
j-lazo <j.org.ls@ciencias.unam.mx>
2022-07-14 18:29:09 +0200

    Circle Houghs option added

--- scripts/general_functions/extract_frames_automatically.py
+++ scripts/general_functions/extract_frames_automatically.py
@@ -104,36 +104,103 @@ def detect_border_and_crop_image(path_image):
     return crop_img
 
 
-def calculate_elipse_hough(image_gray, sigma, high_threshold):
+def calculate_elipse_hough(image_gray, high_threshold=0.6, sigma=3.0):
     edges = canny(image_gray, sigma=sigma,
-                  low_threshold=0.01, high_threshold=high_threshold)
+                  low_threshold=0.05, high_threshold=high_threshold)
     init_time = time.time()
+    plt.figure()
+    plt.imshow(edges)
+    plt.show()
+    min_size = int(min(np.shape(edges))/3)
     # Perform a Hough Transform
     # The accuracy corresponds to the bin size of a major axis.
     # The value is chosen in order to get a single high accumulator.
     # The threshold eliminates low accumulators
-    result = hough_ellipse(edges, accuracy=20, threshold=250,
-                           min_size=350)
+    result = hough_ellipse(edges, accuracy=5, threshold=50,
+                           min_size=min_size)
     delta = time.time() - init_time
     print(delta)
     return result, delta
 
 
-def detect_edge(path_image):
-    image_rgb = cv2.imread(path_image)
+def detect_edge(image_rgb, high_threshold=0.6, sigma=3.0):
+    """#
+
+    image_rgb = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2RGB)
     image_gray = color.rgb2gray(image_rgb)
-    high_threshold = 0.6
-    sigma = 3.0
-    result, delta = calculate_elipse_hough(image_gray, sigma, high_threshold)
+
+    min_radius = int(min(np.shape(image_gray))/4)
+
+    image_gray = color.rgb2gray(image_rgb)
+    edges = canny(image_gray, sigma=2.0,
+                  low_threshold=0.01, high_threshold=0.2)
+
+    plt.figure()
+    plt.imshow(edges)
+    plt.show()
+    print(np.shape(edges))
+    # Perform a Hough Transform
+    # The accuracy corresponds to the bin size of a major axis.
+    # The value is chosen in order to get a single high accumulator.
+    # The threshold eliminates low accumulators
+    result = hough_ellipse(edges, accuracy=20, threshold=20,
+                           min_size=min_radius)
+    result.sort(order='accumulator')
+    print(result)
+    # Estimated parameters for the ellipse
+    best = list(result[-1])
+    yc, xc, a, b = [int(round(x)) for x in best[1:5]]
+    orientation = best[5]
+
+    # Draw the ellipse on the original image
+    cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
+    image_rgb[cy, cx] = (0, 0, 255)
+    # Draw the edge (white) and the resulting ellipse (red)
+    edges = color.gray2rgb(img_as_ubyte(edges))
+    edges[cy, cx] = (250, 0, 0)
+
+    plt.figure()
+    plt.imshow(edges)
+    plt.show()"""
+
+
+
+    # this is in case you want to detect a circle
+    image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)  # color.rgb2gray(image_rgb)
+    min_radius = int(min(np.shape(image_gray))/3)
+
+    circles = cv2.HoughCircles(image_gray, cv2.HOUGH_GRADIENT, 1, min_radius,
+                               param1=80, param2=35,
+                               minRadius=min_radius)
+    #print(circles)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv2.circle(image_rgb, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv2.circle(image_rgb, center, radius, (255, 0, 255), 3)
+
+    plt.figure()
+    plt.imshow(image_rgb)
+    plt.show()
 
     counter = 0
-    while len(result) == 0 and counter < 3:
-        sigma = sigma + 0.05
-        high_threshold = high_threshold - 0.05
-        result, delta = calculate_elipse_hough(image_gray, sigma, high_threshold)
-        counter = counter + 1
-
+    # iterates in case no edge was obtained
+    #while len(result) == 0 and counter < 3:
+    #    print(f' iteration {counter}, sigma:  {sigma}, h. threshold {high_threshold}')
+    #    sigma = sigma + 0.05
+    #    high_threshold = high_threshold - 0.05
+    #   circles = cv2.HoughCircles(image_gray, cv2.HOUGH_GRADIENT, 1, 20,
+    #                              param1=60, param2=40,
+    #                              minRadius=1)
+    #    #result, delta = calculate_elipse_hough(image_gray, sigma=sigma, high_threshold=high_threshold)
+    #    counter = counter + 1
+    """
     if counter == 3:
+        # I don't understant this part???
         image_rgb = np.zeros([2, 2, 2])
         cropped_image = image_rgb
         delta = 999
@@ -150,48 +217,76 @@ def detect_edge(path_image):
         copy_image_rgb = copy.copy(image_rgb)
         image_rgb[cy, cx] = (0, 255, 0)
         cropped_image = crop_image(copy_image_rgb, np.amin(cx), np.amin(cy),
-                                np.amax(cx), np.amax(cy))
+                                np.amax(cx), np.amax(cy))"""
 
+    cropped_image = 0
+    delta = 0
     return image_rgb, cropped_image, delta
 
 
-def extract_frames_video_auto(path_images_folder, extracted_frames_dir, output_dir_edges, output_dir_croped):
+def extract_frames_video_auto(path_video_file, extracted_frames_dir, output_dir=os.getcwd()):
+    path_video_file = os.path.normpath(path_video_file)
+    extracted_frames_dir = os.path.normpath(extracted_frames_dir)
+    output_dir = os.path.normpath(output_dir)
 
-    image_list = sorted([f for f in os.listdir(path_images_folder)
-                  if os.path.isfile(os.path.join(path_images_folder, f))])
-    time_stats = []
-    for image in image_list:
-        print(image)
-        path_image = ''.join([path_images_folder, image])
-        edge_detected, cropped_img, time = detect_edge(path_image)
-        time_stats.append(time)
+    if not (os.path.isfile(path_video_file)):
+        raise f'Video path:{path_video_file} not found'
 
-        if not(np.any(edge_detected)):
-            continue
-        else:
+    image_list = sorted([f for f in os.listdir(extracted_frames_dir)
+                  if os.path.isfile(os.path.join(extracted_frames_dir, f))])
+
+    list_frames = [name.split('frame_')[-1].replace('.png', '') for name in image_list]
+    print(list_frames)
+    name_video = os.path.split(path_video_file)[-1]
+    file_extension = name_video.split('.')[-1]
+    case_id = name_video.replace('.' + file_extension, '')
+
+    cap = cv2.VideoCapture(path_video_file)
+    for j, frame_number in enumerate(list_frames):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, int(frame_number) - 1)
+        image_path = os.path.join(extracted_frames_dir, image_list[j])
+        ret, img = cap.read()
+        image = cv2.imread(image_path)
+        edge_detected, cropped_img, time = detect_edge(img)
+
+
+        #plt.figure()
+        #plt.subplot(121)
+        #plt.imshow(img)
+        #plt.subplot(122)
+        #plt.imshow(image)
+        #plt.show()
+
+   #     count += 1
+    #for image in image_list:
+    #    path_image = os.path.join(extracted_frames_dir, image)
+    #    edge_detected, cropped_img, time = detect_edge(path_image)
+    #    plt.imshow(cropped_img)
+    #    if not(np.any(edge_detected)):
+    #        continue
+    #    else:
             # save the detected edges image
-            cv2.imwrite(''.join([output_dir_edges, image]), edge_detected)
+    #        cv2.imwrite(''.join([output_dir_edges, image]), edge_detected)
             # save the croped images
-            cv2.imwrite(''.join([cropped_img, image]), edge_detected)
+    #        cv2.imwrite(''.join([cropped_img, image]), edge_detected)
 
 
-    csv_file_name = path_images_folder + 'time_test.csv'
+    #csv_file_name = path_images_folder + 'time_test.csv'
 
-    with open(csv_file_name, mode='w') as results_file:
-        results_file_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        results_file_writer.writerow(time_stats)
+    #with open(csv_file_name, mode='w') as results_file:
+    #    results_file_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 
 def main(_argv):
     video_path = FLAGS.video_path
     save_dir = FLAGS.save_dir
     extracted_frames_dir = FLAGS.extracted_frames_dir
-    extract_frames_video_auto(video_path, extracted_frames_dir, save_dir=save_dir)
+    extract_frames_video_auto(video_path, extracted_frames_dir, output_dir=save_dir)
 
 
 if __name__ == '__main__':
     flags.DEFINE_string('video_path', '', 'path of the video')
-    flags.DEFINE_string('save_dir', '', 'path where to save the images extracted')
+    flags.DEFINE_string('save_dir', os.getcwd(), 'path where to save the images extracted')
     flags.DEFINE_string('extracted_frames_dir', '', 'path where frames were previously extracted')
 
     try:
